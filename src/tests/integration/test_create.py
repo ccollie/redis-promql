7c9452317dc2c9957e5b084985934a0bb69791ce
ccollie <gboyah@gmail.com>
2024-09-09 22:41:39 -0400

    Init code.

---
+++ src/tests/integration/test_create.py
@@ -0,0 +1,41 @@
+import math
+import time
+
+import pytest
+import redis
+from RLTest import Env
+from test_helper_classes import SAMPLE_SIZE, _get_ts_info, TSInfo
+from includes import *
+
+def test_create_params():
+    with Env().getClusterConnectionIfNeeded() as r:
+        # test string instead of value
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'RETENTION', 'retention')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'CHUNK_SIZE', 'chunk_size')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'ENCODING')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'ENCODING', 'bad-encoding-type')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'LABELS', 'key', 'val', 'RETENTION', 'abc')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'LABELS', 'key', 'val', 'RETENTION', '-2')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'LABELS', 'key', 'val', 'CHUNK_SIZE', 'abc')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'LABELS', 'key', 'val', 'CHUNK_SIZE', '-2')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'LABELS', 'key', 'val', 'CHUNK_SIZE', '4000000000')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'LABELS', 'key', 'val', 'ENCODING', 'bad-encoding-type')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'LABELS', 'key', 'val', 'DUPLICATE_POLICY', 'bla')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'invalid', 'LABELS', 'key', 'val', 'label', 'DUPLICATE_POLICY', 'bla')
+
+
+        r.execute_command('TS.CREATE', 'a')
+        with pytest.raises(redis.ResponseError):
+            assert r.execute_command('TS.CREATE', 'a')  # filter exists
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -2,3 +2,4 @@ pub mod test_helpers;
 pub mod generators;
 pub mod storage;
 mod utils;
+mod data;
